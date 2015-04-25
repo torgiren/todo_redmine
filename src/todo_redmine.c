59bba88e00e0c0b97df2283fede9ce0f7b83ac38
Marcin Fabrykowski <marcin@fabrykowski.pl>
2015-04-25 05:04:51 +0200

    pobieranie danych

--- src/todo_redmine.c
+++ src/todo_redmine.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<assert.h>
 #include<curl/curl.h>
+#include<jansson.h>
 struct message
 {
     char *str;
@@ -22,11 +24,12 @@ int main()
         fprintf(stderr, "Problem z curl_easy_init()");
         return -1;
     }
+
+    struct message recv_msg = {NULL, 0};
+
     struct curl_slist * headers = NULL;
     headers = curl_slist_append(headers, "Content-Type: application/json");
     headers = curl_slist_append(headers, "X-Redmine-API-Key: 5b1cb3c5ded0b8b2287b6609e95d9e79adc57ce4");
-
-    struct message recv_msg = {NULL, 0};
 
     curl_easy_setopt(curl, CURLOPT_URL, "http://redmine.exphost.pl/issues.json");
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
@@ -49,9 +52,41 @@ int main()
     {
         printf("Nic do wypisania\n");
     }
-    free_msg(&recv_msg);
     curl_slist_free_all(headers);
     curl_easy_cleanup(curl);
+
+    json_t* root;
+    json_error_t error;
+    root = json_loads(recv_msg.str, 0, &error);
+    free_msg(&recv_msg);
+
+    if(!root)
+    {
+        fprintf(stderr, "Problem z json_loads\n%d\t%s\n", error.line, error.text);
+        return -1;
+    }
+    json_t *issues;
+    if((!(issues=json_object_get(root, "issues"))) || (!json_is_array(issues)))
+    {
+        fprintf(stderr, "Blad pobierania danych\n");
+        return -1;
+    }
+    size_t index;
+    json_t *value;
+    json_array_foreach(issues, index, value)
+    {
+        json_t *project, *author;
+        project = json_object_get(value, "project");
+        author = json_object_get(value, "author"); 
+        printf("%d - %s - %s - %s - %s\n",
+                (int)json_integer_value(json_object_get(value,"id")),
+                json_string_value(json_object_get(project,"name")),
+                json_string_value(json_object_get(author,"name")),
+                json_string_value(json_object_get(value,"subject")),
+                json_string_value(json_object_get(value,"created_on"))
+              );
+    }
+    json_decref(root);
     return 0;
 }
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *usrptr)
