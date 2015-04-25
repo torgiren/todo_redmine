464cbe2d1a5d747a15f9088c3ef92766e846c5ad
Marcin Fabrykowski <marcin@fabrykowski.pl>
2015-04-25 03:22:45 +0200

    pierwszy fetch jsona

--- src/todo_redmine.c
+++ src/todo_redmine.c
@@ -2,7 +2,31 @@
 #include<curl/curl.h>
 int main()
 {
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    printf("Hello\n");
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if(curl)
+    {
+        struct curl_slist * headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/xml");
+        headers = curl_slist_append(headers, "X-Redmine-API-Key: 5b1cb3c5ded0b8b2287b6609e95d9e79adc57ce4");
+
+        curl_easy_setopt(curl, CURLOPT_URL, "http://redmine.exphost.pl/issues.xml");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK)
+        {
+            fprintf(stderr, "curl perform error: %s\n", curl_easy_strerror(res));
+            return -1;
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Problem z curl_easy_init()");
+        return -1;
+    }
+    curl_easy_cleanup(curl);
     return 0;
 }
